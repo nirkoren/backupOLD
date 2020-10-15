be988d9d5c62a0f86d348ce810576a5a845cb8ff
Štefan Ivanetič <sivanetic@nil.com>
2020-10-15 16:06:22 +0200

    Add data

--- build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -1,10 +1,14 @@
 package org.nirkoren.maven.demo;
 
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
+
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Dear Developer, is it working from your laptop?";
-		return hello;
+		SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+		Date date = new Date(System.currentTimeMillis());
+		return "Dear Developer, is it working from your laptop?" + formatter.format(date);
 	}
 
 }
